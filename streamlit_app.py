11909b651b13a85ca5414b7e460f2e49f4323bfb
tommyk-mar <tom.kotowski@marceli.eu>
2026-03-11 08:19:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,28 @@
 import streamlit as st
 import pandas as pd
-from streamlit_autorefresh import st_autorefresh
+import time
 
 st.title("Dashboard produkcji")
 
-# automatyczne odświeżanie co 5 sekund
-st_autorefresh(interval=5*1000, key="datarefresh")
-
 csv_file = "https://drive.google.com/uc?id=1U6EadEGw-gFn63lnQTrtv43qtV_H9nnt"
 
-try:
-    # wczytanie CSV
-    df = pd.read_csv(csv_file, delimiter=';')  # delimiter ';' bo Twój CSV ma średniki
+# Streamlit placeholder na tabelę
+placeholder = st.empty()
 
-    st.subheader("Aktualne dane z produkcji")
-    st.dataframe(df)  # wyświetla wszystkie wiersze
+# główna pętla
+while True:
+    try:
+        # wczytanie całego CSV
+        df = pd.read_csv(csv_file, delimiter=';')
 
-except Exception as e:
-    st.error(f"Błąd odczytu pliku: {e}")
+        # wyświetlenie tabeli w Streamlit
+        with placeholder.container():
+            st.subheader("Aktualne dane z produkcji")
+            st.dataframe(df)
+
+    except Exception as e:
+        st.error(f"Błąd odczytu pliku: {e}")
+
+    # odświeżenie co 5 sekund
+    time.sleep(5)
+    st.experimental_rerun()  # wymusza ponowne uruchomienie skryptu i odczyt nowego CSV
