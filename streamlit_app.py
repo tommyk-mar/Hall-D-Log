cd7211d34314fdabac3327c0359843b5a0ed8ef7
tommyk-mar <tom.kotowski@marceli.eu>
2026-03-11 09:05:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("Dynamiczny podgląd CSV bez cache")
 csv_url = "https://drive.google.com/uc?id=1_vjkv3wfZ_Uc8eer8evRVW_j3G5EevQq"
 
 try:
-    df = pd.read_csv(csv_url)
+    df = pd.read_csv(csv_url, sep = ";")
     st.subheader("Tabela CSV")
     st.dataframe(df, width=800, height=400)
     st.table(df)
