cdeb0c67174e7cbf2016896b33c3483059f4e5ac
tommyk-mar <tom.kotowski@marceli.eu>
2026-03-11 09:10:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ csv_url = "https://drive.google.com/uc?id=1_vjkv3wfZ_Uc8eer8evRVW_j3G5EevQq"
 try:
     df = pd.read_csv(csv_url, sep = ";")
     st.subheader("Tabela CSV")
-    st.dataframe(df, width=1600, height=800)
+    st.dataframe(df, width=1600, height=1600)
     st.table(df)
 except Exception as e:
     st.error(f"Nie udało się wczytać pliku CSV: {e}")
