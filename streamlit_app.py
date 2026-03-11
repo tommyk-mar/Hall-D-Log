f4e1dbc1b9f58a3bae9df6a0dd70d62d9bd0f655
tommyk-mar <tom.kotowski@marceli.eu>
2026-03-11 08:04:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# link do CSV
+url = "https://drive.google.com/uc?id=1U6EadEGw-gFn63lnQTrtv43qtV_H9nnt"
+
+st.title("Dashboard produkcji")
+
+# miejsce na dane
+placeholder = st.empty()
+
+while True:
+    try:
+        df = pd.read_csv(url)
+        
+        with placeholder.container():
+            st.subheader("Ostatnie próbki")
+            st.dataframe(df.tail(10))  # ostatnie 10 wierszy
+            
+            st.line_chart(df['produkcja'])  # wykres produkcji
+            st.line_chart(df['odrzut'])    # wykres odrzutów
+
+    except Exception as e:
+        st.error(f"Błąd odczytu pliku: {e}")
+
+    time.sleep(5)  # odświeżanie co 5 sekund
