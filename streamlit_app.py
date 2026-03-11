264cdc58b62fdb6894c34a9cee7f5f153a9f51c8
tommyk-mar <tom.kotowski@marceli.eu>
2026-03-11 10:14:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ csv_url = "https://drive.google.com/uc?id=1_vjkv3wfZ_Uc8eer8evRVW_j3G5EevQq&expo
 
 # Wczytanie oryginalnego CSV
 try:
-    original_df = pd.read_csv(csv_url, sep=";")
+    df = pd.read_csv(csv_url, sep=";")
 except Exception as e:
     st.error(f"Nie udało się wczytać pliku CSV: {e}")
     st.stop()
